f4a4961394674d4ae2895729fda77e983ce59fb8
burhanbeycan <burhanbeycan@hotmail.com>
2025-10-04 17:22:59 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/pva-electrospinning-predictor/',
-  plugins: [react( )],
+  plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
